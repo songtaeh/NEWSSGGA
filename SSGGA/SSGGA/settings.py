916d1d032787fbd9a56da504b05116754af75576
songtaeh <=sjr19660@likelion.org>
2020-06-01 03:06:21 +0900

    0531수정

--- SSGGA/SSGGA/settings.py
+++ SSGGA/SSGGA/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myapp',
     'account',
+    'cafeapp',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'SSGGA.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,7 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 
 LOGIN_REDIRECT_URL = '/'
 
--- SSGGA/SSGGA/urls.py
+++ SSGGA/SSGGA/urls.py
@@ -13,17 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
-from myapp.views import index
+from myapp.views import mainpage
+from cafeapp.views import createcafe
 from account.views import register
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name="index"),
+    path('', mainpage, name="mainpage"),
+    path('createcafe/',createcafe, name="createcafe"),
     path('register/', register, name="register"),
     path('login/', LoginView.as_view(), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
+]   +static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 
-]
+
