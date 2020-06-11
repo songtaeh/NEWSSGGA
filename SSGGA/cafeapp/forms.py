e9293b6ae127a9499f1d27167087fc75224db588
hyunbingil <super199g@naver.com>
2020-06-11 22:25:36 +0900

    pycache ignore

--- SSGGA/cafeapp/forms.py
+++ SSGGA/cafeapp/forms.py
@@ -9,8 +9,26 @@ class CafeForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['Explain'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': "카페를 설명해주세요.",
-            'rows': 10,
-            'cols': 89})
+        self.fields['explain'].widget.attrs.update({
+            'class': 'form_explain',
+            'rows': 5,
+            'cols': 30})
+
+        self.fields['title'].label = "카페명 "
+        self.fields['image'].label = "카페 아이콘 "
+        self.fields['explain'].label = "카페 설명 "
+        self.fields['secret'].label = "공개 여부 "
+        self.fields['tag'].label = "태그 "
+
+        self.fields['title'].widget.attrs.update({
+            'class': "form_title"})    
+        self.fields['tag'].widget.attrs.update({
+            'class': "form_tag"})  
+        self.fields['secret'].widget.attrs.update({
+            'class': "form_secret"
+        })       
+        self.fields['image'].widget.attrs.update({
+            'class': "form_image"})  
+
+
+
--- SSGGA/cafeapp/models.py
+++ SSGGA/cafeapp/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Cafe(models.Model):
     title = models.CharField('카페명',max_length=100)
-    Explain = models.TextField('카페설명',max_length=1000)
+    explain = models.TextField('카페 설명',max_length=1000)
     tag = models.CharField('태그',max_length=30)
     secret = models.BooleanField('공개여부')
-    image = models.ImageField('카페아이콘', null=True, default="./images/userdefaultimg.png")
+    image = models.ImageField('카페 아이콘', null=True, default="./images/userdefaultimg.png")
 
     def __str__(self):
         return self.title
