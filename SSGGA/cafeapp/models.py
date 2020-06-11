2c64131a6a34fcc4cb0b430c4e8654831ef77fda
eunjin917 <cgoya01@likelion.org>
2020-06-11 22:36:12 +0900

    0611

--- SSGGA/cafeapp/models.py
+++ SSGGA/cafeapp/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 class Cafe(models.Model):
     title = models.CharField('카페명',max_length=100)
-    explain = models.TextField('카페 설명',max_length=1000)
+    explain = models.TextField('카페 설명',max_length=1000, null=True)
     tag = models.CharField('태그',max_length=30)
     secret = models.BooleanField('공개여부')
     image = models.ImageField('카페 아이콘', null=True, default="./images/userdefaultimg.png")
