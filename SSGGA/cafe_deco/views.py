c84e33065f4ebb7a54ec10a70285d01b7966494e
JongMyeong Jeong <jmjeong1112@gmail.com>
2020-06-11 19:23:44 +0900

    각 페이지 배너 이미지 추가

--- SSGGA/cafe_deco/views.py
+++ SSGGA/cafe_deco/views.py
@@ -58,9 +58,16 @@ def bulletinboard_page(request):
     else:
         profile = None
 
+    image_obj = BannerImage.objects.last()
+    if image_obj:
+        url = image_obj.image.url
+    else:
+        url = ""
+
     return render(request, 'bulletinboard_page.html', {
         'all_post': all_post,
         'profile': profile,
+        'image' : url,
     })
 
 # def mypost(request):
@@ -74,11 +81,7 @@ def bulletinboard_page(request):
 #     return render(request, 'mypost html 추가예정', {
 #         'all_post': posts,
 #         'profile': profile,
-#     })
-
-
-def change_save(request):
-    return redirect("cafe_main")
+#     }
 
 def user_account(request):
     user_info = Profile.objects.all()
