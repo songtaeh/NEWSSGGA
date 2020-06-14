481276734160471f7bf4d8e5cf387d6309201560
eunjin917 <cgoya01@likelion.org>
2020-06-15 01:38:26 +0900

    0615 배포 준비

--- SSGGA/SSGGA/settings.py
+++ SSGGA/SSGGA/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c$on==3veb*&$_i33zwr&ls!^$d%8=5fz!q!$-m2i-!wf3a4z2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
